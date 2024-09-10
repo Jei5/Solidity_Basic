ea024e67755a10e5121594d99b24b282bf58119d
Jei5 <jeissonfelipe2004@gmail.com>
2024-09-09 19:53:48 -0500

    Se crea el primer contrato de avalanch

---
+++ Hola_mundo.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+//Mi Primer Contrato Hola Mundo
+
+//0xFd0f016c7Bf74Ed0522De7d8BF4594A71a2537da
+
+pragma solidity 0
+.8.24;
+
+contract hola_mundo{
+    string public saludo = "Hola Mundo Desde Avalanche Colombia";
+}
+
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Ejemplo de cambio en remix y que se vea en Github
 
 ##Cambio Github
 
-Ejemplo de Cambio **GitHub** y que se vea en GitHub
+Ejemplo de Cambio **GitHub** y lo veo en Remix
